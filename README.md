723fc66e094cb7b9fd76dbfe7fcf6b54893ed4bb
JulesJux <jules.mercier.fr@gmail.com>
2024-02-10 17:39:25 +0100

    gtk4

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Commande de compilation : \
-gcc -Wall `pkg-config --cflags gtk+-3.0` -o gui -g gnuplot/gnuplot.c charge_condensateur_gtk.c `pkg-config --libs gtk+-3.0` -lm
+gcc -Wall `pkg-config --cflags gtk4` -o gui -g gnuplot/gnuplot.c charge_condensateur_gtk.c `pkg-config --libs gtk4` -lm
 
--- charge_condensateur_gtk.c
+++ charge_condensateur_gtk.c
@@ -1,63 +1,56 @@
-#include <stddef.h>
-#include <gtk/gtk.h>
+#include <gtk-4.0/gtk/gtk.h>
 #include <stdio.h>
 #include <math.h>
 #include "gnuplot/gnuplot.h"
 #include <string.h>
+#define TAILLE_MAX 50
 
-void charge_cond_graph(gnuplot_ctrl *h, double u0, double E, double R, double C, int n){
-    double X[n];
-    double Y[n];
-    for (int i = 0; i < n; i++){
-        X[i] = i;
-        Y[i] = (u0-E)*exp(-i/(R*C)) + E;
-    }
-    gnuplot_cmd(h, "set term wxt persist");
-    gnuplot_setterm(h, "wxt", 1920, 1080);
-    gnuplot_plot_coordinates(h, X, Y, n, "Réponse échelon de tension (circuit RC)");
+void charge_cond(gnuplot_ctrl *h, char* u0, char* E, char* R, char* C) {
+    char cmd[TAILLE_MAX] = {};
+    strcat(cmd, "plot (");
+    strcat(cmd, u0);
+    strcat(cmd, "-");
+    strcat(cmd, E);
+    strcat(cmd, ")*exp(-x/(");
+    strcat(cmd, R);
+    strcat(cmd, "*");
+    strcat(cmd, C);
+    strcat(cmd, "))+");
+    strcat(cmd, E);     //plot (Uc(0)-E)exp(-x/RC) + E
+
+    gnuplot_setterm(h, "wxt", 720, 480);
+    gnuplot_cmd(h, cmd);
     gnuplot_close(h);
-}
-
-static void
-print_hello(GtkWidget *widget,
-            gpointer data) {
-    g_print("Hello World\n");
 }
 
 static void show_graph (GtkWidget *widget,
             gpointer data) {
-    double u0, E, R, C;
-    scanf("%lf", &u0);
-    scanf("%lf", &E);
-    scanf("%lf", &R);
-    scanf("%lf", &C);
-    gnuplot_ctrl *h;
-    h = gnuplot_init();
-    charge_cond_graph(h, u0, E, R, C, 50);
+    char u0[TAILLE_MAX], E[TAILLE_MAX], R[TAILLE_MAX], C[TAILLE_MAX];
+    scanf("%s", u0);
+    scanf("%s", E);
+    scanf("%s", R);
+    scanf("%s", C);
+    gnuplot_ctrl *h = gnuplot_init();
+    charge_cond(h, u0, E, R, C);
 }
 
 static void
 activate(GtkApplication *app,
          gpointer user_data) {
-    GtkWidget *window;
-    GtkWidget *button;
-    GtkWidget *button_box;
 
+    GtkBuilder *builder = gtk_builder_new ();
+    gtk_builder_add_from_file (builder, "tipe.ui", NULL);
 
-
-    window = gtk_application_window_new(app);
+    GObject *window = gtk_builder_get_object (builder, "window");
     gtk_window_set_title(GTK_WINDOW(window), "Window");
+    gtk_window_set_application (GTK_WINDOW (window), app);
     gtk_window_set_default_size(GTK_WINDOW(window), 1920, 1080);
 
-    button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
-    gtk_container_add(GTK_CONTAINER(window), button_box);
-
-    button = gtk_button_new_with_label("Hello World");
-    g_signal_connect(button, "clicked", G_CALLBACK(show_graph), NULL);
+    GObject *button = gtk_builder_get_object (builder, "button1");
+    g_signal_connect(button, "clicked", G_CALLBACK (show_graph), NULL);
    // g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
-    gtk_container_add(GTK_CONTAINER(button_box), button);
-
-    gtk_widget_show_all(window);
+    gtk_widget_set_visible (GTK_WIDGET (window), TRUE);
+    g_object_unref (builder);
 }
 
 int
@@ -65,7 +58,6 @@ main(int argc,
      char **argv) {
     GtkApplication *app;
     int status;
-
     app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
