8525860dc643191bea206cf20231405be476ad93
JulesJux <jules.mercier.fr@gmail.com>
2024-04-11 10:43:05 +0200

    add grpah.h

---
+++ graph/graph.h
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
+struct maillon_s {
+    int a; 
+    int b;
+    struct maillon_s* next; 
+};
+typedef struct maillon_s maillon;
+
+struct s_graph{
+int* sommet ;
+int nbs;
+maillon* aretes ; 
+};
+typedef struct s_graph graph;
+
+
+
+int** mult(int** m1,int** m2,int t);
+graph* construit(int S);
+void libere_m(maillon* M );
+void libere_graph(graph* G);
+graph* ajoute_S(graph* G);
+void ajoute_A(graph* G,int a,int b);
+bool estdansgraph(graph* G,int n);
+bool estvoisin(graph* G,int u ,int v);
+bool estarete(graph* G,int a,int b);
+bool estchemin(graph* G,maillon* chemin);
+bool existechemin(graph* G,int u,int v,int etape);
+bool estconexe(graph* G);
+int* voisins(graph* G,int s);
+int** adjacence (graph* G);
+int ** mat_adj(graph* G);
+
